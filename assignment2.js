d23af4ec73ce6f5de6f00a489d0ba4087a678ef4
ButterUdder <Geoffrey.Huang27@myhunter.cuny.edu>
2021-10-03 11:21:51 -0400

    implementations for forEach and Push

---
+++ assignment2.js
@@ -0,0 +1,72 @@
+// FOR EACH //
+Array.prototype.myEach = function(callbackFn) {
+	for(let i = 0; i < this.length; i++) {
+		if(this[i] === undefined) continue;
+		//Element
+		//Element, index
+		//element, index, Array
+		callbackFn(this[i], i, this);
+	}
+}
+
+// MAP //
+Array.prototype.myMap = function() {
+
+};
+
+// FILTER //
+Array.prototype.myFilter = function() {
+
+};
+
+// SOME //
+Array.prototype.mySome = function() {
+
+};
+
+// EVERY //
+Array.prototype.myEvery = function() {
+
+};
+
+// REDUCE //
+Array.prototype.myReduce = function() {
+
+};
+
+// INCLUDES //
+Array.prototype.myIncludes = function() {
+
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function() {
+
+};
+
+// PUSH //
+Array.prototype.myPush = function (...args) {
+	let arg_i = 0;
+	let length = this.length;
+	//last element of 'this' is at length - 1
+	for(let i = length; i < length + args.length; i++) {
+		this[i] = args[arg_i];
+		arg_i++;
+	}
+	return this.length;
+}
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function() {
+
+};
+
+// KEYS //
+Object.grabKeys = function() {
+
+};
+
+// VALUES //
+Object.grabValues = function() {
+
+};

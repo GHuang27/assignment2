f8cbc09b92bcb35747892fce840ed29f9f652b3d
ButterUdder <Geoffrey.Huang27@myhunter.cuny.edu>
2021-10-04 14:52:29 -0400

    removed testing lines for mySome

--- assignment2.js
+++ assignment2.js
@@ -56,13 +56,6 @@ Array.prototype.mySome = function(callbackFn) {
 	return false;
 };
 
-/*
-const arr = ["spray", "limit", "elite", "exuberant", "destruction", "present", , , , , ];
-const even = (element) => element % 2 === 0;
-console.log(arr.mySome(word => word.length > 6));
-console.log(arr.some(word => word.length > 6));
-*/
-
 // EVERY //
 Array.prototype.myEvery = function(callbackFn) {
 	for(let i = 0; i < this.length; i++) {
